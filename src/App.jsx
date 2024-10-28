88f2c9dd6ad16c18dcecd376bff1f7e8f76347dd
nsavva38 <nsavva38@gmail.com>
2024-10-28 14:31:05 -0400

    displays puppy info and netlify attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import { puppyList } from './data'
 import { useState } from 'react';
+import './index.css'
 import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(`puppies passed into array:`, puppies);
+  const [featPupId, setFeatPupId] = useState(null)
+
+
+  function handleClick() {
+    // some logic here
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
 
   return (
     <div>
       { 
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         }) 
       }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
